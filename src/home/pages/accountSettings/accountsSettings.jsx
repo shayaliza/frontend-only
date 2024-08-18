6769d5e74ffb6fd98cc2f6ea7f851928526fef97
shayaliza <magicalmind01@gmail.comgit statuss>
2024-08-18 16:59:32 +0530

    generate new token

--- src/home/pages/accountSettings/accountsSettings.jsx
+++ src/home/pages/accountSettings/accountsSettings.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getAllData } from "../../../fetching/decodingJwt";
 import { useToast } from "@/components/ui/use-toast";
+import { Button } from "@mui/material";
+import refreshTokenFetch from "../../../fetching/Interceptor/refresh";
 
 function AccountSettings() {
   const { toast } = useToast();
@@ -19,14 +21,14 @@ function AccountSettings() {
     setDecodedItem(decoded);
   }, [reduxAccessToken]);
 
+  const handleGenerateToken = () => {
+    const token = refreshTokenFetch(reduxRefreshToken);
+    console.log(token);
+  };
   return (
     <div className="p-6 bg-gray-100 min-h-screen flex flex-col items-center">
       <h1 className="text-2xl font-bold mb-6">Account Settings</h1>
-      <div
-        onClick={() => toast({ title: "Raise Toast", variant: "destructive" })}
-      >
-        Raise Toast
-      </div>
+
       <div className="w-full bg-white shadow-md rounded-lg p-4 mb-4">
         <div className="text-lg font-semibold text-gray-700 mb-2">
           Refresh Token
@@ -56,6 +58,9 @@ function AccountSettings() {
               : "No data available"}
           </pre>
         </div>
+        <Button variant="contained" onClick={handleGenerateToken}>
+          Generate New Token
+        </Button>
       </div>
     </div>
   );
