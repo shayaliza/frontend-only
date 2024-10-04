ccb079eb74a2fd70dd5004b553e858d310de3511
shayaliza <magicalmind01@gmail.comgit statuss>
2024-10-04 22:20:19 +0530

    dekstop logo dropdown

--- src/home/component/layoutComp/header.jsx
+++ src/home/component/layoutComp/header.jsx
@@ -58,26 +58,26 @@ const Header = ({ toggleMobileMenu }) => {
                 <IoIosArrowDropdown size={24} />
               </button>
               {isDropdownOpen && (
-              <div className="absolute top-12 -right-10 mt-2 w-48  bg-black text-white bg-opacity-25 backdrop-blur-md shadow-lg  rounded-lg z-[999]">
-                <ul className="list-none p-2 m-0">
-                  <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
-                    Follower
-                  </li>
-                  <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
-                    Following
-                  </li>
-                  <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
-                    <Link to="/dashboard/profile">Home</Link>
-                  </li>
-                  <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
-                    <Link to="/createsnap/analytics">Createsnap</Link>
-                  </li>
-                  <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
-                    <Link to="/datasnap">Datasnap</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
+                <div className="absolute top-12 -right-10 mt-2 w-48  bg-black text-white bg-opacity-25 backdrop-blur-md shadow-lg  rounded-lg z-[999]">
+                  <ul className="list-none p-2 m-0">
+                    <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                      Follower
+                    </li>
+                    <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                      Following
+                    </li>
+                    <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                      <Link to="/dashboard/profile">Home</Link>
+                    </li>
+                    <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                      <Link to="/createsnap/analytics">Createsnap</Link>
+                    </li>
+                    <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                      <Link to="/datasnap">Datasnap</Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
           <div className="flex items-center mr-4">
@@ -105,12 +105,37 @@ const Header = ({ toggleMobileMenu }) => {
       style={{ boxShadow: "0 6px 16px -1px rgba(0, 0, 0, 0.3)" }}
     >
       <div className="relative mx-auto flex justify-between items-center max-[900px]:min-h-[45px]">
-        <div className="flex items-center">
+        <div
+          className="flex items-center"
+          ref={dropdownRef}
+          onClick={toggleDropdown}
+        >
           <img
             src={Image}
             alt="Logo"
             className="w-32 h-12 object-contain hidden md:block"
           />
+          {isDropdownOpen && (
+            <div className="absolute top-16 left-2 mt-2 w-48  bg-black text-white bg-opacity-25 backdrop-blur-md shadow-lg  rounded-lg z-[999]">
+              <ul className="list-none p-2 m-0">
+                <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                  Follower
+                </li>
+                <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                  Following
+                </li>
+                <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                  <Link to="/dashboard/profile">Home</Link>
+                </li>
+                <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                  <Link to="/createsnap/analytics">Createsnap</Link>
+                </li>
+                <li className="p-2 hover:bg-gray-400 hover:text-gray-900 cursor-pointer">
+                  <Link to="/datasnap">Datasnap</Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
         <div className="flex items-center mr-4">
           <div className="Icons flex items-center min-[900px]:mr-4 justify-end">
