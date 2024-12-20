89fe615a9abc04b875c46202c48b0090b2f19927
Vigneshreddy <vigneshreddy625@gmail.com>
2024-12-21 00:54:08 +0530

    added

--- src/datasnap/components/Effects/CommentBottomSheet.jsx
+++ src/datasnap/components/Effects/CommentBottomSheet.jsx
@@ -89,7 +89,7 @@ const CommentBottomSheet = ({ isOpen, onClose, comments = [], profileImg }) => {
 
             <form 
               onSubmit={handleSubmit}
-              className={`border-t p-4 flex items-center space-x-3 shrink-0 ${keyboardHeight ? `absolute bottom-${keyboardHeight} left-0 right-0` : ''}`}>
+              className={`border-t p-4 flex items-center space-x-3 shrink-0 ${keyboardHeight ? `fixed bottom-${keyboardHeight} left-0 right-0` : ''}`}>
               <img 
                 src={profileImg} 
                 alt="Your profile" 
